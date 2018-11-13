c5b922509a73f25c874edfcbd8cb8adf7c74f361
egrzeda <ethangrzeda@gmail.com>
2018-11-13 15:33:52 -0500

    Laying thing out
    This is just some expansion on the groundwork

--- transp.c
+++ transp.c
@@ -4,3 +4,26 @@
 * I pledge my honor that I have abided by the Stevens Honor System.
 * Ethan Grzeda
 */
+
+/*
+* NOTES for this project
+*
+* - This code will be the basis of problem 2 (transpif.c)
+* - Use locality principles in inner loops
+* - change return type when possible or when you know what the return type should be
+*/
+
+int main(){
+	return 0;
+}
+
+/*
+* void printmatrix
+*
+* This function is only to be used for testing purposes in order to view a matrix in the works
+*
+* - change parameters
+*/
+void printmatrix(){
+	return 0;
+}
