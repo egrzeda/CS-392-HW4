42e24d2387cf2163d23b6f1acdf9737455e3de03
egrzeda <ethangrzeda@gmail.com>
2018-11-13 16:09:20 -0500

    more groundwork
    Created the random matrix function and laid out some more notes

--- transp.c
+++ transp.c
@@ -13,6 +13,10 @@
 * - change return type when possible or when you know what the return type should be
 */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
 	return 0;
 }
@@ -26,4 +30,17 @@ int main(){
 */
 void printmatrix(){
 	return 0;
+}
+
+/*
+* float *randomatrix
+*
+* returns a randomly created matrix of floats in the form of a 1-dimensional array with the
+* length being the square of the width inputted
+*
+* - make sure to create another input for this for the problem 2 version
+* - are there any limits as to what floats can be created here, or is it just completely random?
+*/
+float *randomatrix(int width){
+	
 }
