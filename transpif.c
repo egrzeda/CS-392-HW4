ae448d0f6e087f924030eb56408c2884001f92d5
egrzeda <ethangrzeda@gmail.com>
2018-11-18 11:18:13 -0500

    Finished Porting
    Copied over much more from transp.c to transpif.c and made the code able to compile and run for powers of 2

--- transpif.c
+++ transpif.c
@@ -10,11 +10,20 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-void printmatrix(float *matrix, int width);
+/*
+* This is the structure for the outlines of the block to be transposed
+*/
+struct block{
+	int row;
+	int col;
+	int width;
+};
 
-float *randomatrix(int width);
+void printmatrix(float *matrix, int height, int width);
 
-void transposeblock(struct block tb, float **ogmat, float **tmat, int wofmat);
+float *randomatrix(int height, int width);
+
+void transposeblock(struct block tb, float **ogmat, float **tmat, int hofmat, int wofmat);
 
 /*
 * int main
@@ -68,4 +77,70 @@ int main(int argc, char **argv){
 	free(tmat);
 
 	return 0;
+}
+
+/*
+* void transposeblock
+*
+* transposes the elements of one block from the original matrix into the new matrix
+*
+* Note: throughout this whole function, the height and width of the matrix are used in reverse
+* for the transposed matrix since the dimensions are going to be different if it's not square.
+*/
+void transposeblock(struct block tb, float **ogmat, float **tmat, int hofmat, int wofmat){
+
+	/* I think this following section should be the same for this as in transp.c */
+	int wofbloc = tb.width;
+	int startctr = wofbloc * tb.col;
+	int startrtc = wofbloc * tb.row;
+	int coltorow;
+	int rowtocol;
+
+	/* besides the boundary check, I think this should be the same as in transp.c */
+	for(int d = 0; d < wofbloc; d++){
+		for(int e = 0; e < wofbloc; e++){
+			/* INSERT BOUNDARY CHECK IN HERE; PROBABLY IN AN IF STATEMENT */
+			coltorow = startctr + d;
+			rowtocol = startrtc + e;
+			int tmposition = coltorow * wofmat + rowtocol;
+			int omposition = rowtocol * wofmat + coltorow;
+			*(*tmat + tmposition) = *(*ogmat + omposition);
+		}
+	}
+}
+
+/*
+* void printmatrix
+*
+* This function is only to be used for testing purposes in order to view a matrix in the works. It
+* does not assume that the matrix is square in this instance, unlike the one in transp.c.
+*
+*/
+void printmatrix(float *matrix, int height, int width){
+	printf("The height is %i\n", height);
+	printf("The width is %i\n", width);
+	for(int b = 0; b < height * width; b++){
+		if(b % width == 0 && b != 0){
+			printf("\n");
+		}
+		printf("%f ", matrix[b]);
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
+/*
+* float *randomatrix
+*
+* returns a randomly created matrix of floats in the form of a 1-dimensional array with the
+* length being the product of the height and width inputted. These floats are between -100 and
+* 100, by the way
+*
+*/
+float *randomatrix(int height, int width){
+	float *randoms = malloc(sizeof(float) * height * width);
+	for(int a = 0; a < height * width; a++){
+		randoms[a] = (float)((float)rand() / RAND_MAX * 200-100);
+	}
+	return randoms;
 }
